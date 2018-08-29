61295a90c7d2c59d793228973db178374fe3c937
Andy <isongliang@qq.com>
2018-08-30 01:37:47 +0800

    project init

---
+++ weibo/src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class='home'>
+    <div >home1</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+  .home {
+    font-size: 50px;
+  }
+</style>
